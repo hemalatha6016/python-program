34e8d687bae0b2cf1c1018b0f17b1ed9f3d276d4
hemalatha6016 <hemalathas_bec28@mepcoeng.ac.in>
2026-04-14 22:15:13 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,13 @@
+#Read in two integers, a and b, and print three lines.
+The first line is the integer division a//b (While using Python2 remember to import division from __future__).
+The second line is the result of the modulo operator:a%b .
+The third line prints the divmod of a and b.
+
+
+def divmod_task():
+    a=int(input())
+    b=int(input())   
+    print(a//b)
+    print(a%b)
+    print(divmod(a,b))
+divmod_task()
